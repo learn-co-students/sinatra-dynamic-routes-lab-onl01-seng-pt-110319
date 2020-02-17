ca9db1386d93043e99f27cc9c03be836806b8e0d
wills <richwblake@gmail.com>
2020-02-17 14:19:15 -0500

    Done.

--- app.rb
+++ app.rb
@@ -3,4 +3,48 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  get '/reversename/:name' do
+    @name = params[:name]
+    "Your name in reverse is #{@name.reverse}"
+  end
+
+  get '/square/:number' do
+    @number = params[:number]
+    "The square of #{@number} is #{@number.to_i ** 2}."
+  end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    @final_string = ""
+
+    @number.times do
+      @final_string += "#{@phrase}"
+    end
+
+    @final_string
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+
+    case params[:operation]
+    when "add"
+      "#{@number1 + @number2}"
+    when "subtract"
+      "#{@number1 - @number2}"
+    when "multiply"
+      "#{@number1 * @number2}"
+    when "divide"
+      "#{@number1 / @number2}"
+    else
+      "Not a valid operation, please try again!"
+    end
+  end
+
 end
